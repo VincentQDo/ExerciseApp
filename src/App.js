9d8cb59206fd4be4ee4a5a8d7d9f19391d621c32
Vincent <QD899836@wcupa.edu>
2020-01-04 00:19:48 -0500

    Removed white space between paralax and content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Welcome from "./components/Welcome/Welcome";
+import Content from "./components/MainContent/Content";
 import "./style/App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App parallax">
       <NavBar />
       <Welcome />
+      <Content />
     </div>
   );
 }
---
+++ src/components/MainContent/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default Content;
