f6f50684700e25e482a47234798c814d6528b6c5
Vincent <QD899836@wcupa.edu>
2020-01-09 22:43:50 -0500

    added exercise api content to exercise page

--- src/components/ExercisePage.js
+++ src/components/ExercisePage.js
@@ -2,12 +2,36 @@ import React from "react";
 import "./Generic.css";
 import "../style/App.css";
 
-function ExercisePage() {
-  return (
-    <div className="nav-padding parallax">
-      <h1>Test file</h1>
-    </div>
-  );
+class ExercisePage extends React.Component {
+  state = {
+    dataJson: null,
+    stateDataDescription: null,
+    loading: true
+  };
+  async componentDidMount() {
+    const url = "https://wger.de/api/v2/exerciseinfo/";
+    const response = await fetch(url);
+    const responseData = await response.json();
+    this.setState({ loading: false, data: responseData });
+    const dataDescription = this.state.data.results.map(
+      item => item.description
+    );
+    this.setState({ stateDataDescription: dataDescription });
+    console.log(dataDescription);
+    // this.setState({ data: dataDescription });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <div>loading...</div>
+        ) : (
+          <div>{this.state.stateDataDescription}</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default ExercisePage;
