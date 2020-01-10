36cf2a6a087a848bc5dca6b53cc17fab4da13cc0
Vincent <QD899836@wcupa.edu>
2020-01-09 22:31:23 -0500

    Added dynamic type and update style

--- src/components/InputsWithAnimation/InputFields.js
+++ src/components/InputsWithAnimation/InputFields.js
@@ -4,7 +4,12 @@ import "./InputFields.css";
 const InputFields = props => {
   return (
     <div className="input-form">
-      <input type="text" name={props.field} autoComplete="off" required></input>
+      <input
+        type={props.type}
+        name={props.field}
+        autoComplete="off"
+        required
+      ></input>
       <label htmlFor={props.field} className="label-name">
         <span className="content-name">{props.field}</span>
       </label>
