b5baa5c5f41fdca6cdf3523470f60cb849dc472e
Vincent <QD899836@wcupa.edu>
2020-01-10 00:00:16 -0500

    Added comments for more clarity and TODO

--- src/components/ExercisePage.js
+++ src/components/ExercisePage.js
@@ -8,11 +8,17 @@ class ExercisePage extends React.Component {
     stateDataDescription: null,
     loading: true
   };
+
+  //? async and await are new topics need to learn more, also promises are new need to learn that too
+  //* componentDidMount is here to get rid of an error (need to learn more about what it does)
   async componentDidMount() {
+    // using let instead of const to allow the data to change in the while loop below
+    //TODO need to figure out a way to not repeat commands again in the while loop below
     let url = "https://wger.de/api/v2/exerciseinfo/";
     let response = await fetch(url);
     let responseData = await response.json();
-
+    // while looop to loop through all the pages of the API
+    //TODO add pagination to the page someshow to easy navigation through api data, maybe download data to a json?
     while (responseData.next) {
       console.log(responseData);
       url = responseData.next;
