9fa1cfa31669d47e6473aa55e54dc50b5a70d67d
Vincent <QD899836@wcupa.edu>
2020-01-10 22:59:50 -0500

    successfully call api to get valid exercise data

--- src/components/ExercisePage.js
+++ src/components/ExercisePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Generic.css";
 import "../style/App.css";
 import "./ExercisePage.css";
+import ExercisePagination from "./ExercisePagination";
 
 class ExercisePage extends React.Component {
   state = {
@@ -15,23 +16,33 @@ class ExercisePage extends React.Component {
   async componentDidMount() {
     // using let instead of const to allow the data to change in the while loop below
     //TODO need to figure out a way to not repeat commands again in the while loop below
-    let url = "https://wger.de/api/v2/exerciseinfo/";
+    let url = "https://wger.de/api/v2/exercise/?status=2&language=2";
     let response = await fetch(url);
     let responseData = await response.json();
     // while looop to loop through all the pages of the API
     //TODO add pagination to the page someshow to easy navigation through api data, maybe download data to a json?
-    while (responseData.next) {
-      console.log(responseData);
-      url = responseData.next;
-      response = await fetch(url);
-      responseData = await response.json();
-    }
+    // while (responseData.next) {
+    //   console.log(responseData);
+    //   url = responseData.next;
+    //   response = await fetch(url);
+    //   responseData = await response.json();
+    // }
 
     this.setState({ loading: false, data: responseData });
     // map each item description into a div
-    const dataDescription = this.state.data.results.map(item => (
-      <div className="alert alert-success">{item.description}</div>
-    ));
+    const dataDescription = this.state.data.results.map(item => {
+      console.log(typeof item.description);
+      console.log(item.description);
+      return (
+        <div>
+          <div className="black-text">{item.name}</div>
+          <div
+            className="alert alert-success"
+            dangerouslySetInnerHTML={{ __html: item.description }}
+          />
+        </div>
+      );
+    });
     this.setState({ stateDataDescription: dataDescription });
   }
 
@@ -43,6 +54,7 @@ class ExercisePage extends React.Component {
         ) : (
           <div>
             <div>{this.state.stateDataDescription}</div>
+            <ExercisePagination />
           </div>
         )}
       </div>
