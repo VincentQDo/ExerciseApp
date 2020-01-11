31ecf6ad75e895374c7833b0bac6b44f4a2add67
Vincent <QD899836@wcupa.edu>
2020-01-10 23:42:44 -0500

    Made better looking cards

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
+import "./ExerciseCard.css";
 
 export default class ExerciseCard extends Component {
   render() {
     const style = { width: "18rem" };
     return (
-      <div className="card" style={style}>
-        <img src="..." className="card-img-top" alt="..." />
+      <div className="card vw-100">
+        <div
+          className="card-header black-text"
+          dangerouslySetInnerHTML={{ __html: this.props.name }}
+        />
         <div className="card-body">
-          <h5
-            className="card-title"
-            dangerouslySetInnerHTML={{ __html: this.props.name }}
-          ></h5>
-          <p
-            className="card-text"
-            dangerouslySetInnerHTML={{ __html: this.props.description }}
-          />
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <blockquote className="blockquote mb-0">
+            <div
+              className="black-text-children"
+              dangerouslySetInnerHTML={{ __html: this.props.description }}
+            />
+          </blockquote>
         </div>
       </div>
     );
